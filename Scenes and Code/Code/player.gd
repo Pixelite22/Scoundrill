1477b427b7dc894ac28a5f7453f115f9911dd147
Pixelite22 <114366972+Pixelite22@users.noreply.github.com>
2026-02-20 19:42:38 -0500

    UPDATE 8
    Changed health to a bar and the current weapon to a display

--- Scenes and Code/Code/player.gd
+++ Scenes and Code/Code/player.gd
@@ -6,19 +6,36 @@ extends Control
 var active_weapon : weaponCard = null
 
 @onready var health_label: RichTextLabel = $Health
+@onready var health_Bar: ProgressBar = $Health/Bar
+@onready var health_value: RichTextLabel = $Health/Value
+
 @onready var weapon_power_label: RichTextLabel = $"Weapon Power"
+@onready var weapon_card: Control = $"Weapon Power/Weapon Card"
+
 @onready var highest_number_label: RichTextLabel = $"Highest Number"
+@onready var deck_remaining: RichTextLabel = $"Deck Remaining"
 
 
 var effect := ["NONE", "BURNT", "FROZEN", "POISON"]
 
 func _process(_delta: float) -> void:
+	handle_health()
+	handle_weapon()
+
+func handle_health():
 	if health > 20: #If health ever goes above 20
 		health = 20 #Set it to 20
 	if health < 0: #If health ever goes below 0
 		health = 0 #Set it to 0
 	
-	health_label.text = "Health: " + str(health) #Set the Text showing health on the screen to health and the value
+	health_Bar.value = health #Set the bar showing health on the screen to health value
+	health_value.text = str(health)
+
+func handle_weapon():
 	if active_weapon != null: #If there is an active weapon
-		weapon_power_label.text = "Weapon Power: " + str(active_weapon.value)#Display it's strength
-		highest_number_label.text = "The highest number card you can attack with your weapon is: " + str(active_weapon.kill_cap)
+#		weapon_power_label.text = "Weapon Power: " + str(active_weapon.value)#Display it's strength
+		highest_number_label.text = "The Highest number your weapon can attack: " + str(active_weapon.kill_cap)
+
+
+func deck_info(deck_resource):
+	deck_remaining.text = "Cards left in deck: " + str(deck_resource.cards.size())
--- Scenes and Code/Custom Resources/card.gd
+++ Scenes and Code/Custom Resources/card.gd
@@ -18,3 +18,8 @@ func resolve(card : Card, player):
 #Function for applying the effect of a card, currently handled in individiual card resources
 func apply_effect(card : Card):
 	pass
+
+#Function asking permssion for use of weapon if weapon is equipped
+func permission(want_to_use):
+	pass
+	
